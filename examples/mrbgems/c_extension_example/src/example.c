35df753e73db8a995049913c570db39a313dc26e
Jun Hiroe <Jun.Hiroe@gmail.com>
2013-07-05 00:00:07 +0900

    Create CExtestion Class and instance method.

--- examples/mrbgems/c_extension_example/src/example.c
+++ examples/mrbgems/c_extension_example/src/example.c
@@ -8,10 +8,22 @@ mrb_c_method(mrb_state *mrb, mrb_value self)
   return self;
 }
 
+mrb_value my_class_print_name(mrb_state* mrb, mrb_value self)
+{
+  mrb_int n;
+
+  mrb_get_args(mrb, "i", &n);
+
+  printf("my_class_print_name method n=%d\n", n);
+  return self;
+}
+
 void
 mrb_c_extension_example_gem_init(mrb_state* mrb) {
-  struct RClass *class_cextension = mrb_define_module(mrb, "CExtension");
-  mrb_define_class_method(mrb, class_cextension, "c_method", mrb_c_method, MRB_ARGS_NONE());
+  //struct RClass *class_cextension = mrb_define_module(mrb, "CExtension");
+  //mrb_define_class_method(mrb, class_cextension, "c_method", mrb_c_method, MRB_ARGS_NONE());
+  struct RClass *my_class = mrb_define_class(mrb, "MyClass", mrb->object_class);
+  mrb_define_method(mrb, my_class, "print_name", my_class_print_name, MRB_ARGS_REQ(1));
 }
 
 void
