a473c67c2290b14bd53b7053a9015d20eeb253fc
Jun Hiroe <Jun.Hiroe@gmail.com>
2013-09-01 10:35:15 +0900

    Add super in aliased methods.

--- src/vm.c
+++ src/vm.c
@@ -1054,12 +1054,25 @@ mrb_context_run(mrb_state *mrb, struct RProc *proc, mrb_value self, unsigned int
       mrb_callinfo *ci = mrb->c->ci;
       struct RProc *m;
       struct RClass *c;
+      struct RClass *target_class;
+      struct RProc *method;
       mrb_sym mid = ci->mid;
       int a = GETARG_A(i);
       int n = GETARG_C(i);
 
       recv = regs[0];
       c = mrb->c->ci->target_class->super;
+      /* mrb_method_search_vm func changes a given class. */
+      target_class = mrb->c->ci->target_class;
+      method = mrb_method_search_vm(mrb, &target_class, mid);
+      if (MRB_PROC_ALIAS_METHOD_P(method)) {
+        /* Change an original method name from an aliased method name */
+        mrb_value alias = mrb_obj_iv_get(mrb, (struct RObject*)mrb->c->ci->target_class, mrb_intern_lit(mrb, "alias"));
+        mrb_value org_mid = mrb_hash_get(mrb, alias, mrb_symbol_value(mid));
+        if (!mrb_nil_p(org_mid)) {
+          mid = mrb_obj_to_sym(mrb, org_mid);
+        }
+      }
       m = mrb_method_search_vm(mrb, &c, mid);
       if (!m) {
         mid = mrb_intern_lit(mrb, "method_missing");
