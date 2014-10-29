3ff210dc857580c9baefa7743f089a88522da8e9
Jun Hiroe <Jun.Hiroe@gmail.com>
2014-10-29 23:43:36 +0900

    Update mruby-redis path

--- build_config.rb
+++ build_config.rb
@@ -19,7 +19,7 @@ MRuby::Build.new do |conf|
   # conf.gem :github => 'masuidrive/mrbgems-example', :checksum_hash => '76518e8aecd131d047378448ac8055fa29d974a9'
   # conf.gem :git => 'git@github.com:masuidrive/mrbgems-example.git', :branch => 'master', :options => '-v'
 
-  conf.gem '/Users/hiroejun/work/mruby-redis/mruby-redis'
+  conf.gem '/Users/hiroejun/repo/src/github.com/suzukaze/mruby-redis'
 
   # include the default GEMs
   conf.gembox 'default'
